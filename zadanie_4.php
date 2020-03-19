01dd668b4910f4bbab0fe8dd0cb335a2ea40128d
Sergeev O.V <o.v.sergeyev@gmail.com>
2020-03-19 14:55:15 +0300

    Р”РѕРјР°С€РЅРµРµ Р·Р°РґР°РЅРёРµ Рє РїРµСЂРІРѕРјСѓ СѓСЂРѕРєСѓ

---
+++ zadanie_4.php
@@ -0,0 +1,18 @@
+<?php
+    $title = 'Задание N4';
+    $h1    = 'H1 заголовок';
+    $year  = date('Y');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo($title); ?></title>
+</head>
+<body>
+    <h1><?php echo($h1); ?></h1>
+    <p>
+        Текущий год: <?php echo($year); ?>
+    </p>
+</body>
+</html>
